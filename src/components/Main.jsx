658b892d923569590ba6f051d6854175566c85d0
tonioreign <tonioreign@gmail.com>
2022-06-14 14:03:54 -0700

    ADDED: Post styling - starting REDUX

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -28,6 +28,59 @@ const Main = () => {
                     </button>
                 </div>
             </ShareBox>
+            <div>
+                <Article>
+                    <SharedActor>
+                        <a>
+                            <img src="./images/user.svg" alt="user" />
+                            <div>
+                                <span>Title</span>
+                                <span>Info</span>
+                                <span>Date</span>
+                            </div>
+                        </a>
+                        <button>
+                            <img src="./images/dots.png" alt="dots" />
+                        </button>
+                    </SharedActor>
+                    <Description>Description</Description>
+                    <SharedImg>
+                        <a>
+                            <img src="/images/thunder.jpg" alt="image" />
+                        </a>
+                    </SharedImg>
+                    <SocialCounts>
+                        <li>
+                            <button>
+                                <img src="/images/like.png" alt="like" />
+                                <img src="/images/clapping.png" alt="" />
+                                <span>75</span>
+                            </button>
+                        </li>
+                        <li>
+                            <a>2 comments</a>
+                        </li>
+                    </SocialCounts>
+                    <SocialActions>
+                        <button>
+                            <img src="./images/like.png" />
+                            <span>Like</span>
+                        </button>
+                        <button>
+                            <img src="./images/comment.png" />
+                            <span>Comment</span>
+                        </button>
+                        <button>
+                            <img src="./images/share.png" />
+                            <span>Share</span>
+                        </button>
+                        <button>
+                            <img src="./images/send.png" />
+                            <span>Send</span>
+                        </button>
+                    </SocialActions>
+                </Article>
+            </div>
         </Container>
     );
 };
@@ -103,6 +156,135 @@ const ShareBox = styled(CommonCard)`
                     color: #70b5f9;
                 }
             }
+        }
+    }
+`;
+
+const Article = styled(CommonCard)`
+    padding: 0;
+    margin: 0 0 8px;
+    overflow: visible;
+`;
+
+const SharedActor = styled.div`
+    padding-right: 40px;
+    flex-wrap: nowrap;
+    padding: 12px 16px 0;
+    margin-bottom: 8px;
+    align-items: center;
+    display: flex;
+
+    a {
+        margin-right: 12px;
+        flex-grow: 1;
+        overflow: hidden;
+        display: flex;
+        text-decoration: none;
+
+        img {
+            width: 48px;
+            height: 48px;
+            border-radius: 50%;
+        }
+
+        & > div {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            flex-basis: 0;
+            margin-left: 8px;
+            overflow: hidden;
+
+            span {
+                text-align: left;
+                &:first-child {
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: rgba(0, 0, 0, 1);
+                }
+
+                &:nth-child(n + 1) {
+                    font-size: 12px;
+                    color: rgba(0, 0, 0, 0.6);
+                }
+            }
+        }
+    }
+
+    button {
+        position: absolute;
+        right: 12px;
+        top: 0;
+        background: transparent;
+        border: none;
+        outline: none;
+    }
+`;
+
+const Description = styled.div`
+    padding: 0 16px;
+    overflow: hidden;
+    color: rgba(0, 0, 0, 0.9);
+    font-size: 14px;
+    text-align: left;
+`;
+const SharedImg = styled.div`
+    margin-top: 8px;
+    width: 100%;
+    display: block;
+    position: relative;
+    background-color: #f9fafb;
+    img {
+        object-fit: contain;
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const SocialCounts = styled.ul`
+    line-height: 1.3;
+    display: flex;
+    align-items: flex-start;
+    overflow: auto;
+    margin: 0 16px;
+    padding: 8px 0;
+    border-bottom: 1px solid #e9e5df;
+    list-style: none;
+
+    img {
+        width: 16px;
+        height: 16px;
+    }
+    li {
+        margin-right: 5px;
+        font-size: 12px;
+        button {
+            display: flex;
+        }
+    }
+`;
+
+const SocialActions = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 0;
+    min-height: 48px;
+    padding: 4px 8px;
+
+    button {
+        display: inline-flex;
+        align-items: center;
+        padding: 8px;
+        color: #0a66c2;
+
+        img {
+            width: 48px;
+            height: 48px;
+        }
+
+        @media (min-width: 768px) {
+            margin-left: 8px;
         }
     }
 `;
